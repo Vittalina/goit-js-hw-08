bcfb44a3dbf62fdb2daaedb97399600a0ff5f886
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-06-10 14:36:15 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const galleryItem = galleryItems.map(({preview, original, description}) => 
+    `<li>
+<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`)
+        .join("");
+    
+gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+
+let galleryEl = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+});
