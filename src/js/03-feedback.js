3f9a3e15ad5d251737a5c504cbcda436204ad272
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-06-13 13:59:28 +0300

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,8 +3,6 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = "feedback-form-state";
 const KEY = "feedback-message";
 
-const formData = {};
-
 const refs = {
     form: document.querySelector("form"),
     input: document.querySelector("input"),
@@ -15,23 +13,13 @@ refs.form.addEventListener("submit", onFormSubmit);
 refs.input.addEventListener("input", throttle(onEmailInput, 500));
 refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
 
-refs.form.addEventListener("input", event => {
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-    
-    formData[event.target.name] = event.target.value;
-    
-    console.log(formData);
-    const formDataJSON = JSON.stringify(formData);
-    console.log(formDataJSON);
-})
-
-populateTextarea();
+populateInputs();
 
 function onFormSubmit(event) {
     event.preventDefault();
     event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+    localStorage.removeItem(KEY);
 }
 
 function onEmailInput(event) {
@@ -45,7 +33,7 @@ function onTextareaInput(event) {
     localStorage.setItem(KEY, message);
 }
 
-function populateTextarea() {
+function populateInputs() {
     const savedMessage = localStorage.getItem(KEY);
     const savedEmail = localStorage.getItem(STORAGE_KEY);
 
