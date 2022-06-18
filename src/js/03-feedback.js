eb2c9c1ce6f226b31c00eeca5cf8d6892e358e9c
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-06-18 14:21:53 +0300

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,8 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = "feedback-form-state";
 
-const formData = {};
+const formData = {
+    };
 
 const refs = {
     form: document.querySelector("form"),
@@ -13,33 +14,27 @@ const refs = {
 refs.form.addEventListener("submit", onFormSubmit);
 
 refs.form.addEventListener("input", throttle(event => {
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-    
     formData[event.target.name] = event.target.value;
-
-    // console.log(formData);
     const formDataJSON = JSON.stringify(formData);
-    // console.log(formDataJSON);
     localStorage.setItem(STORAGE_KEY, formDataJSON);
-    // console.log(formDataJSON);
 }, 500));
 
-populateTextarea();
+populateInput();
 
 function onFormSubmit(event) {
     event.preventDefault();
     event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
     console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
 }
 
-function populateTextarea() {
+function populateInput() {
     const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (savedText) {
     const [email, message] = refs.form.elements;
-    email.value = savedText.email || '';
+        email.value = savedText.email || '';
         message.value = savedText.message || '';
+    }
 }
-}
+
